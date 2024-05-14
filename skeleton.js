b0389b4511851b3e4f433de1967ba5ba242e5d5f
jagboka <jagboka@icloud.com>
2024-05-14 18:07:33 -0400

    Finished game! Sloppy, but works as intended.

--- skeleton.js
+++ skeleton.js
@@ -2,6 +2,32 @@ console.log("Linking Test!")
 
 var compChoice = ""
 var userChoice = ""
+let compScore = 0
+let userScore = 0
+
+
+//Outputs for getComputerChoice to return
+var a = "rock"
+var b = "paper"
+var c = "scissors"
+
+for (let i = 0; i < 4; i++) {
+
+//Function for user choice
+function getHumanChoice () {
+    let q = prompt("Let's Play!")
+
+    if (q == "r") {
+        userChoice = "rock"
+        return "You chose rock!"
+    } else if (q == "p") {
+        userChoice = "paper"
+        return "You chose paper!"
+    } else if (q == "s") {
+        userChoice = "scissors"
+        return "You chose scissors!"
+    }
+}
 
 //Computer choice generated via random numbers
 function getComputerChoice (a, b, c) {
@@ -20,42 +46,33 @@ function getComputerChoice (a, b, c) {
     }
 }
 
-//Outputs for getComputerChoice to return
-var a = "rock"
-var b = "paper"
-var c = "scissors"
 
-//Function for user choice
-function getHumanChoice () {
-    let q = prompt("Let's Play!")
-
-    if (q == "r") {
-        userChoice = "rock"
-        return "You chose rock!"
-    } else if (q == "p") {
-        userChoice = "paper"
-        return "You chose paper!"
-    } else if (q == "s") {
-        userChoice = "scissors"
-        return "You chose scissors!"
-    }
-}
-
-function testr () {
+function playRound () {
     if ((userChoice == "rock" && compChoice == "rock") || (userChoice == "paper" && compChoice == "paper") || (userChoice == "scissors" && compChoice == "scissors")) {
         return "you tied"
     }
 
     if ((userChoice == "rock" && compChoice == "scissors") || (userChoice == "paper" && compChoice == "rock") || (userChoice == "scissors" && compChoice == "paper")) {
+        userScore++
         return "you won"
     }
 
     if ((userChoice == "rock" && compChoice == "paper") || (userChoice == "paper" && compChoice == "scissors") || (userChoice == "scissors" && compChoice == "rock")) {
+        compScore++
         return "comp won"
     }
 }
 
-console.log(getComputerChoice(a, b, c))
 console.log(getHumanChoice())
-console.log(testr())
+console.log(getComputerChoice(a, b, c))
+console.log(playRound())
+console.log("comp score: " + compScore)
+console.log("user score: " + userScore)
+}
 
+
+if (userScore > compScore) {
+    console.log("Congrats! You won the game")
+} else if (userScore < compScore) {
+    console.log("Computer won the game!")
+}
